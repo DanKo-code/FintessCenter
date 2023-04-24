947695976150ad79efcabba54de95e640869e2bc
Tanki1629762003 <96771888+Tanki1629762003@users.noreply.github.com>
2023-04-24 18:14:33 +0300

    сделал тупой, но рабочий слайдер

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -7,19 +7,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace FitnessCenter.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
+        List<string> SliderImages = new List<string>();
+        int SliderImagesIndex = 0;
+
         #region Accessors (helpers for ui design)
 
         #region SliderImage
-        int temp = Application.Current.Resources.Values.Count;
-
-        ICollection<object> list = Application.Current.Resources.MergedDictionaries[1];
-
-        private string _sliderImage = "dfvdf";
+        private string _sliderImage;
 
         public string SliderImage
         {
@@ -36,31 +36,63 @@ namespace FitnessCenter.ViewModel
         }
         #endregion
 
-
-
         #endregion
 
         #region Commands
 
-        //#region ShowLogin
-        //public ICommand ShowLoginCommand { get; }
+        #region LeftImageCpmmand
+        public ICommand LeftImageCpmmand { get; }
 
-        //private bool CanShowLoginCommand(object p) => true;
+        private bool CanLeftImageCommand(object p) => true;
 
-        //private void OnShowLoginCommand(object p)
-        //{
-        //    LoginVisibility = Visibility.Visible;
-        //    RegisterVisibility = Visibility.Collapsed;
-        //}
-        //#endregion
+        private void OnLeftImageCommand(object p)
+        {
+            if ((--SliderImagesIndex) < 0)
+            {
+                SliderImagesIndex = SliderImages.Count - 1;
+            }
 
+            SliderImage = SliderImages[SliderImagesIndex];
+        }
+        #endregion
 
+        #region RightImageCpmmand
+        public ICommand RightImageCpmmand { get; }
+
+        private bool CanRightImageCommand(object p) => true;
+
+        private void OnRightImageCommand(object p)
+        {
+            if ((++SliderImagesIndex) == (SliderImages.Count))
+            {
+                SliderImagesIndex = 0;
+            }
+
+            SliderImage = SliderImages[SliderImagesIndex];
+        }
+        #endregion
 
         #endregion
 
         public MainViewModel()
         {
-            //ShowLoginCommand = new RelayCommand(OnShowLoginCommand, CanShowLoginCommand);
+            LeftImageCpmmand = new RelayCommand(OnLeftImageCommand, CanLeftImageCommand);
+            RightImageCpmmand = new RelayCommand(OnRightImageCommand, CanRightImageCommand);
+
+            //Найти словарь
+            var imagesDict = Application.Current.Resources.MergedDictionaries.FirstOrDefault(d => d.Source?.OriginalString == "/Resources/ImagesDictionary/Images.xaml");
+            
+            //Переписать с него данные
+            if (imagesDict != null)
+            {
+                foreach (string key in imagesDict.Values)
+                {
+                    SliderImages.Add(key);
+                    // обрабатываем каждый элемент в словаре
+                }
+            }
+
+            SliderImage = SliderImages[0];
         }
     }
 }
